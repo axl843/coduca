a548750d4d81d0414b45af6bd2b19fcaa33e9c3c
Stanislav Pashchenko <axl84@MacBook-Pro-Stanislav.local>
2019-12-25 14:23:24 +0200

    commit

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,7 +5,7 @@ import {
 } from "mdbreact";
 import { Link } from 'gatsby';
 import CustomNavLink from './customLink';
-import { ReactComponent as Logo } from "../images/light-bulb.svg";
+import { ReactComponent as Logo } from "../images/logo-coduca.svg";
 
 class NavbarPage extends Component {
   state = {
