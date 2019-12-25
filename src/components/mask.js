c74c6ff690b2f579f69c6bac0efecdfc384cc956
Stanislav Pashchenko <axl84@MacBook-Pro-Stanislav.local>
2019-12-25 13:53:13 +0200

    commit

--- src/components/mask.js
+++ src/components/mask.js
@@ -8,8 +8,8 @@ const Intro = ({ children }) => {
                 <MDBCol md="12" className="white-text text-center">
                     <h2 className="h1-responsive font-weight-bold white-text mb-0 pt-md-5 pt-5">Coduca88 Digital Agency</h2>
                     <hr className="hr-light my-4 w-75" />
-                    <h4 className="subtext-header h4-responsive mt-2 mb-4">Gimme your money.</h4>
-                    <MDBBtn color="info" ><MDBIcon icon="home" className="mr-2" /> Visit us</MDBBtn>
+                    <h4 className="subtext-header h4-responsive mt-2 mb-4">You Deserve a Trusted Partner.</h4>
+                    {/* <MDBBtn color="info" ><MDBIcon icon="home" className="mr-2" /> Visit us</MDBBtn> */}
                 </MDBCol>
             </MDBRow>
         </MDBContainer>
